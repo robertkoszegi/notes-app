cc9f425ad907e106fc56c6996a67c8cec095b385
robertkoszegi <robkoszegi@gmail.com>
2021-08-17 22:28:45 -0400

    removing env, changes to extras

--- controllers/notes.js
+++ controllers/notes.js
@@ -160,14 +160,6 @@ async function updateList(req, res, next) {
 }
 
 
-
-
-// function addList(req, res, next) {
-//     // req.user.facts.push(req.body);
-//     // req.user.save(function(err) {
-//     //   res.redirect('/students');
-//     // });
-// }
   
 // -------------------
 // DELETE RECORD
